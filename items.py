1566266b6317640b7d4f0f7c4703ace694690ad5
Isdsar <isdsarinath@gmail.com>
2017-10-26 04:36:41 +0100

    Add files via upload

--- items.py
+++ items.py
@@ -3,7 +3,7 @@ item_vodka = {
 
     "name" : "bottle of Vodka",
 
-    "description": "",
+    "description": "Not your favorite drink, but it's about as much alcohol as you can fit in one bottle. You'll impress with this that's for sure. It just needs something to mix it with.",
 
     "abilities" : {}
 }
@@ -13,7 +13,7 @@ item_coke = {
 
     "name" : "bottle of Coke",
 
-    "description" : "",
+    "description" : "The sugary goodness that everybody loves, nobody is going to notice this bottle missing. All you need is a spirit and you're set for the party. Well, as far as drinks go. ",
 
     "abilities" : {}
 }
@@ -23,7 +23,7 @@ item_backpack = {
 
     "name" : "backpack",
 
-    "description" : "",
+    "description" : "Your old school rucksack, you haven't seen this thing since year 7. You thought it was cool but nobody else did. This will help you to carry the things you need to escape.",
 
     "abilities" : {}
 }
@@ -43,7 +43,7 @@ item_rope = {
 
     "name" : "rope",
 
-    "description" : "",
+    "description" : "An old but strong rope. This sinews got a date with a window, and you've got a date with a party.",
 
     "abilities" : {}
 }
@@ -53,7 +53,7 @@ item_laptop = {
 
     "name" : "laptop",
 
-    "description" : "",
+    "description" : "You can't bring your music along without this.",
 
     "abilities" : {}
 }
@@ -63,7 +63,7 @@ item_flashlight = {
 
     "name" : "flashlight",
 
-    "description" : "",
+    "description" : "How else are you going to venture into dark and scary places?",
 
     "abilities" : {}
 }
@@ -73,8 +73,7 @@ item_flamethrower = {
 
     "name" : "flamethrower",
 
-    "description" : "",
-
+    "description" : "Just like you saw your mate John do out the back of the school shed, who knew deodeorant could be so fun?! Flamethrower is an understatement."
     "abilities" : {}
 }
 
@@ -83,7 +82,7 @@ item_lighter = {
 
     "name" : "lighter",
 
-    "description" : "",
+    "description" : "You don't smoke, that would just be silly. But a bit of deodeorant and this might help you get through that basement!",
 
     "abilities" : {"merge" : {"deodorant" : item_flamethrower}}
 }
@@ -93,7 +92,7 @@ item_deodorant = {
 
     "name" : "deodorant",
 
-    "description" : "",
+    "description" : "You smell fine, but you can't help but notice the big flammable icon on the can.",
 
     "abilities" : {"merge" : {"lighter" : item_flamethrower}}
 }
@@ -103,7 +102,7 @@ item_glasses = {
 
     "name" : "pair of glasses",
 
-    "description" : "",
+    "description" : "You weren't gifted with the best eyesight. But thanks to these you can actually see.",
 
     "abilities" : {}
 }
@@ -113,7 +112,7 @@ item_watch = {
 
     "name" : "watch",
 
-    "description" : "",
+    "description" : "Would you look at the time! Time to get to the party. Better not take too long trying to get there.",
 
     "abilities" : {}
 }
